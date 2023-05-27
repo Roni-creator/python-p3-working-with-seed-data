3f28663ceae3342ad0b66c855fde61789bd92ffb
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-05-27 15:50:23 -0700

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -16,4 +16,11 @@ if __name__ == '__main__':
     session = Session()
 
 
-    import ipdb; ipdb.set_trace()
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    session.add(botw)
+    session.commit()
+
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    session.bulk_save_objects([ffvii, mk8])
+    session.commit()
